81f93161d9c78ad9bfadd99584aece689cc39cdf
amandaguan-ag <amandaguan1314@gmail.com>
2024-03-23 01:14:50 -0700

    Connect Frontend with backend and create a Todo Module in NestJS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Button } from "@chakra-ui/react";
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+const sendTodo = () => {
+  axios
+    .post("http://localhost:3000/todos", {
+      title: "New Todo",
+      description: "A hardcoded Todo item.",
+    })
+    .then((response: AxiosResponse<any>) => console.log(response))
+    .catch((error: AxiosError) => console.error(error));
+};
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
         <header className="App-header">
+          <Button onClick={sendTodo}>Add Todo</Button>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
