43f7aea616daab312240efd493324df48aecded9
amandaguan-ag <amandaguan1314@gmail.com>
2024-03-27 00:18:54 -0700

    seperate completed tasks to bottom of the page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ChakraProvider, Box, VStack } from "@chakra-ui/react";
+import { ChakraProvider, Box, VStack, Heading } from "@chakra-ui/react";
 import axios from "axios";
 import TaskList from "./components/TaskList";
 import AddTask from "./components/AddTask";
@@ -17,14 +17,14 @@ const App: React.FC = () => {
   const fetchTasks = async () => {
     try {
       const response = await axios.get("http://localhost:3005/tasks");
-      const fetchedTasks: Task[] = response.data; 
+      const fetchedTasks: Task[] = response.data;
 
-      const sortedTasks = fetchedTasks.sort((a: Task, b: Task) => {
+      fetchedTasks.sort((a, b) => {
         const priorityOrder = { High: 1, Medium: 2, Low: 3 };
         return priorityOrder[a.priority] - priorityOrder[b.priority];
       });
 
-      setTasks(sortedTasks);
+      setTasks(fetchedTasks);
     } catch (error) {
       console.error("Failed to fetch tasks:", error);
     }
@@ -38,12 +38,28 @@ const App: React.FC = () => {
     fetchTasks();
   };
 
+  const completedTasks = tasks.filter((task) => task.completed);
+  const notCompletedTasks = tasks.filter((task) => !task.completed);
+
   return (
     <ChakraProvider>
       <Box m={10}>
         <VStack spacing={8}>
           <AddTask onTasksUpdated={handleRefreshTasks} />
-          <TaskList onTasksUpdated={handleRefreshTasks} tasks={tasks} />
+          <Heading size="md" mb={4}>
+            Not Completed Tasks
+          </Heading>
+          <TaskList
+            onTasksUpdated={handleRefreshTasks}
+            tasks={notCompletedTasks}
+          />
+          <Heading size="md" mb={4}>
+            Completed Tasks
+          </Heading>
+          <TaskList
+            onTasksUpdated={handleRefreshTasks}
+            tasks={completedTasks}
+          />
         </VStack>
       </Box>
     </ChakraProvider>
