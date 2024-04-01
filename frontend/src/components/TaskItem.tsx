a00f3f9b84bf0099987c3bcdae1d7a9fd87d08c5
amandaguan-ag <amandaguan1314@gmail.com>
2024-04-01 00:01:32 -0700

    Add useBreakpointValue hook to TaskItem component

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Checkbox, ListItem, Button, Text, Grid, Box } from "@chakra-ui/react";
+import {
+  Checkbox,
+  ListItem,
+  Button,
+  Text,
+  Grid,
+  Box,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { Task } from "../types/Task";
 import EditTaskModal from "./EditTaskModal";
@@ -61,6 +69,8 @@ const TaskItem: React.FC<TaskProps> = ({ task, onToggle, isHighlighted }) => {
     return "transparent";
   };
 
+  const baseLayout = useBreakpointValue({ base: true, md: false });
+
   return (
     <>
       <EditTaskModal
@@ -78,7 +88,16 @@ const TaskItem: React.FC<TaskProps> = ({ task, onToggle, isHighlighted }) => {
           }
         />
         <Grid
-          templateColumns="repeat(3, 1fr) auto auto"
+          templateColumns={baseLayout ? "1fr" : "repeat(3, 1fr) auto auto"}
+          templateRows={baseLayout ? "auto auto auto" : "auto"}
+          templateAreas={{
+            base: `"checkbox"
+                   "description"
+                   "priority"
+                   "date"
+                   "edit-delete"`,
+            md: `"checkbox description priority date edit-delete"`,
+          }}
           gap={4}
           alignItems="center"
           py={2}
@@ -86,17 +105,40 @@ const TaskItem: React.FC<TaskProps> = ({ task, onToggle, isHighlighted }) => {
           borderWidth={isHighlighted ? "2px" : "1px"}
           borderColor={isHighlighted ? "yellow.300" : "gray.200"}
         >
-          <Checkbox isChecked={task.completed} onChange={toggleCompletion}>
-            {task.description}
+          <Checkbox
+            isChecked={task.completed}
+            onChange={toggleCompletion}
+            gridArea="checkbox"
+          >
+            <Text fontWeight="bold" gridArea="description">
+              {task.description}
+            </Text>
           </Checkbox>
-          <Text fontWeight="bold">{task.priority} Priority</Text>
-          <Text color="#6B6666">{formatDate(task.createdAt)}</Text>
-          <Button bg="#C83E3E" color="white" size="sm" onClick={deleteTask}>
-            Delete
-          </Button>
-          <Button size="sm" onClick={handleOpenEditModal}>
-            Edit
-          </Button>
+          <Text fontWeight="bold" gridArea="priority">
+            {task.priority} Priority
+          </Text>
+          <Text color="#6B6666" gridArea="date">
+            {formatDate(task.createdAt)}
+          </Text>
+          <Box
+            gridArea="edit-delete"
+            display="flex"
+            justifyContent="end"
+            mt={baseLayout ? 2 : 0}
+          >
+            <Button
+              bg="#C83E3E"
+              color="white"
+              size="sm"
+              onClick={deleteTask}
+              mr={2}
+            >
+              Delete
+            </Button>
+            <Button size="sm" onClick={handleOpenEditModal}>
+              Edit
+            </Button>
+          </Box>
         </Grid>
       </ListItem>
     </>
