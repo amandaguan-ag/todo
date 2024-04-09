4e189845129eb451c7716ac64221e485f5eb85fe
Amanda Guan <121919044+amandaguan-ag@users.noreply.github.com>
2024-04-08 22:18:29 -0700

    reset migration history (#11)

--- backend/src/migrations/1712639648105-migrations.ts
+++ backend/src/migrations/1712639648105-migrations.ts
@@ -1,10 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddTagsToTasks1711933963848 implements MigrationInterface {
-    name = 'AddTagsToTasks1711933963848'
+export class Migrations1712639648105 implements MigrationInterface {
+    name = 'Migrations1712639648105'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "tag" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_8e4052373c579afc1471f526760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "task" ("id" SERIAL NOT NULL, "description" character varying NOT NULL, "completed" boolean NOT NULL DEFAULT false, "priority" character varying NOT NULL DEFAULT 'Medium', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "task_tags_tag" ("taskId" integer NOT NULL, "tagId" integer NOT NULL, CONSTRAINT "PK_28bdc8d6452f65a8ae3f4c2ab25" PRIMARY KEY ("taskId", "tagId"))`);
         await queryRunner.query(`CREATE INDEX "IDX_374509e2164bd1126522f424f6" ON "task_tags_tag" ("taskId") `);
         await queryRunner.query(`CREATE INDEX "IDX_0e31820cdb45be62449b4f69c8" ON "task_tags_tag" ("tagId") `);
@@ -18,6 +19,7 @@ export class AddTagsToTasks1711933963848 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX "public"."IDX_0e31820cdb45be62449b4f69c8"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_374509e2164bd1126522f424f6"`);
         await queryRunner.query(`DROP TABLE "task_tags_tag"`);
+        await queryRunner.query(`DROP TABLE "task"`);
         await queryRunner.query(`DROP TABLE "tag"`);
     }
 
