b9b70b88a1bdda9dde37f2e74e332c47c41c4152
amandaguan-ag <amandaguan1314@gmail.com>
2024-04-27 14:41:37 -0700

    Add authentication guard and user details endpoint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 import { Transform } from 'class-transformer';
 import * as sanitizeHtml from 'sanitize-html';
+import { AuthGuard } from './auth.guard';
 
 export class SignUpDto {
   @IsEmail()
@@ -20,5 +28,14 @@ export class AuthController {
   @Post('sign-up')
   signUp(@Body() body: SignUpDto) {
     return this.authService.signUp(body);
+  }
+  @UseGuards(AuthGuard)
+  @Get('user-details')
+  getUser(@Request() req) {
+    if (req.user) {
+      return req.user;
+    } else {
+      return 'no user';
+    }
   }
 }
