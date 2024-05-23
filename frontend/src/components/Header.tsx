ef80c9edf9d4d5dc31848a4d462e6659894d43ab
amandaguan-ag <amandaguan1314@gmail.com>
2024-05-22 23:08:02 -0700

    refactor: Update page name in Header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const pages = [
   { name: "Log In", path: "/log-in" },
   { name: "Sign Up", path: "/sign-up" },
-  { name: "Todo", path: "/home" },
+  { name: "Task Manager", path: "/home" },
 ];
 
 const Header = () => {
